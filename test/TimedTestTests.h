a0c164c469d1d92b0ae05b181277d41eb85c80a7
Sqeaky <sqeaky@blacktoppstudios.com>
2018-02-02 01:02:16 -0600

    Loosed timing and commented on it.

--- test/TimedTestTests.h
+++ test/TimedTestTests.h
@@ -63,7 +63,8 @@ SILENT_TEST_GROUP(WarningTimedTestTests, WarningTimedTest)
 BENCHMARK_TEST_GROUP(TimedTestTests, TimedTest)
 {
     // Positive tests This should serve as examples for how to use this and get tests that passed.
-    TEST_TIMED("TestTimedPassing", std::chrono::milliseconds(200), std::chrono::milliseconds(60),
+    // These amounts of time very short to be measuring this way. longer running tests can be more precise.
+    TEST_TIMED("TestTimedPassing", std::chrono::milliseconds(200), std::chrono::milliseconds(100),
             []{ std::this_thread::sleep_for( std::chrono::milliseconds(200) ); });
 
     TEST_TIMED_UNDER("TestTimedUnderPassing", std::chrono::microseconds(5000), []{ });
