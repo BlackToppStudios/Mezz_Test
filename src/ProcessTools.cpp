76ef770f833e08cd36b5c92dfb4189c83395cca5
MakoEnergy <blackwoodj@blacktoppstudios.com>
2021-12-16 16:50:30 -0600

    Reverting changes.

--- src/ProcessTools.cpp
+++ src/ProcessTools.cpp
@@ -292,7 +292,7 @@ namespace {
     /// @return Returns a String suitable for invoking the system shell with the given command.
     [[nodiscard]]
     String CreateShellCommand(const StringView Command)
-        { return String{"sh -ic "}.append(Command); }
+        { return String{"sh -c "}.append(Command); }
 
     // Move this out of the if/else should windows actually need it.
     /// @brief Extracts just the file system path to the executable from a complete command.
